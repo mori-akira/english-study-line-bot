02ba20f75411b9da3dae3a6f82fbbdee561a8c16
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-02-02 22:26:53 +0900

    Update lambda_function.py

--- api/EnglishStudyBotPushQuestion/lambda_function.py
+++ api/EnglishStudyBotPushQuestion/lambda_function.py
@@ -2,6 +2,6 @@ from openai_query import generate_question  # type: ignore
 from linebot_query import push_message  # type: ignore
 
 
-def lambda_handler():
+def lambda_handler(event, context):
     question = generate_question()
     push_message(question, "Uc1d198055dfe857defef8679cf21e8a3")
