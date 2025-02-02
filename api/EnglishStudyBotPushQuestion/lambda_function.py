52d6026c55b915e3b3d2db9a638e92c90683abb3
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-02-02 22:31:47 +0900

    Update lambda_function.py

--- api/EnglishStudyBotPushQuestion/lambda_function.py
+++ api/EnglishStudyBotPushQuestion/lambda_function.py
@@ -5,7 +5,7 @@ from linebot_query import push_message  # type: ignore
 def lambda_handler(event, context):
     question = generate_question()
     message = (
-        "【英訳問題】以下の和文を英訳してください。"
+        "【英訳問題】以下の和文を英訳してください。\n\n"
         f"{question}"
     )
     push_message(message, "Uc1d198055dfe857defef8679cf21e8a3")
