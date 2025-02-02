e423ed38defb661414f93f26ec1bff52e9356ac0
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-02-03 08:59:51 +0900

    Update

--- api/EnglishStudyBotPushQuestion/lambda_function.py
+++ api/EnglishStudyBotPushQuestion/lambda_function.py
@@ -1,11 +1,41 @@
-from openai_query import generate_question  # type: ignore
+import boto3
+from mypy_boto3_dynamodb.service_resource import DynamoDBServiceResource
+from boto3.dynamodb.conditions import Attr
+
+from openai_query import UserInfo, generate_question  # type: ignore
 from linebot_query import push_message  # type: ignore
 
 
+dynamodb: DynamoDBServiceResource = boto3.resource("dynamodb")  # type: ignore
+user_table = dynamodb.Table("english-study-bot-user")
+quiz_table = dynamodb.Table("english-study-bot-quiz-history")
+
+
 def lambda_handler(event, context):
-    question = generate_question()
-    message = (
-        "【英訳問題】以下の和文を英訳してください。\n\n"
-        f"{question}"
+    # ユーザ一覧取得
+    users = []
+    response = user_table.scan(
+        FilterExpression=Attr('is_valid').eq(True)
     )
-    push_message(message, "Uc1d198055dfe857defef8679cf21e8a3")
+    users.extend(response.get("Items", []))
+    while "LastEvaluatedKey" in response:
+        response = user_table.scan(
+            FilterExpression=Attr('is_valid').eq(True)
+        )
+        users.extend(response.get("Items", []))
+
+    # 各ユーザに出題
+    for user in users:
+        question = generate_question(UserInfo(
+            english_word_count_indication=int(
+                user.get("english_word_count_indication")
+            ),
+            english_level=user.get("english_level"),
+            purpose=user.get("purpose"),
+            occupation=user.get("occupation"),
+        ))
+        message = (
+            "【英訳問題】以下の和文を英訳してください。\n\n"
+            f"{question}"
+        )
+        push_message(message, user.get("user_id"))
