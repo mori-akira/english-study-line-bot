be4bc465d05aa84f6f0ddb7f0ac7551dba0e0e31
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-02-03 10:20:59 +0900

    Update lambda_function.py

--- api/EnglishStudyBotFeedback/lambda_function.py
+++ api/EnglishStudyBotFeedback/lambda_function.py
@@ -4,7 +4,7 @@ from linebot_query import handle_message
 def lambda_handler(event, context):
     headers = event.get("headers", {})
     print(headers)
-    signature = headers.get("X-Line-Signature")
+    signature = headers.get("x-line-signature")
     body = event.get("body", "{}")
     print(body)
     handle_message(body, signature)
